a54dc3ab79c510965ecb826809aebe3b40b1bb55
tm-fuad05 <tm.fuad05@gmail.com>
2024-12-31 20:39:29 +0600

    all  artifact if no data found show empty added

--- src/Pages/AllArtifacts.jsx
+++ src/Pages/AllArtifacts.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 import React, { useEffect, useState } from "react";
 import ArtifactCard from "../Layouts/ArtifactCard";
-import empty from "../assets/empty.jpg";
 
 const AllArtifacts = () => {
   const [allArtifacts, setAllArtifacts] = useState([]);
@@ -19,20 +18,11 @@ const AllArtifacts = () => {
         Artifacts: {allArtifacts.length}
       </h2>
       <hr />
-      {allArtifacts.length === 0 ? (
-        <div className="w-7/12 mx-auto my-20">
-          <img src={empty} alt="" />
-          <h2 className="text-2xl lg:text-3xl font-bold text-gray-500 text-center">
-            No Data Found
-          </h2>
-        </div>
-      ) : (
-        <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {allArtifacts.map((artifact) => (
-            <ArtifactCard artifact={artifact}></ArtifactCard>
-          ))}
-        </div>
-      )}
+      <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {allArtifacts.map((artifact) => (
+          <ArtifactCard artifact={artifact}></ArtifactCard>
+        ))}
+      </div>
     </div>
   );
 };
