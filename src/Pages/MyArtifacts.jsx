7f5208fd1f76e512da7cbbd930459cd6afca294a
tm-fuad05 <tm.fuad05@gmail.com>
2024-12-31 20:38:19 +0600

    my liked  artifact if no data found show empty added

--- src/Pages/MyArtifacts.jsx
+++ src/Pages/MyArtifacts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import MyArtifactCArd from "../Layouts/MyArtifactCard";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
+import empty from "../assets/empty.jpg";
 
 const MyArtifacts = () => {
   const { user } = useAuth();
@@ -20,16 +21,25 @@ const MyArtifacts = () => {
           My Artifacts: {myAddedArtifacts.length}
         </h2>
         <hr />
-        <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {myAddedArtifacts.map((myArtifact) => (
-            <MyArtifactCArd
-              key={myArtifact._id}
-              myArtifact={myArtifact}
-              myAddedArtifacts={myAddedArtifacts}
-              setMyAddedArtifacts={setMyAddedArtifacts}
-            ></MyArtifactCArd>
-          ))}
-        </div>
+        {myAddedArtifacts.length === 0 ? (
+          <div className="w-7/12 mx-auto my-20">
+            <img src={empty} alt="" />
+            <h2 className="text-2xl lg:text-3xl font-bold text-gray-500 text-center">
+              No Data Found
+            </h2>
+          </div>
+        ) : (
+          <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {myAddedArtifacts.map((myArtifact) => (
+              <MyArtifactCArd
+                key={myArtifact._id}
+                myArtifact={myArtifact}
+                myAddedArtifacts={myAddedArtifacts}
+                setMyAddedArtifacts={setMyAddedArtifacts}
+              ></MyArtifactCArd>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
